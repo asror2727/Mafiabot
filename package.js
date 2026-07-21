8d825da50b2854253686776ecd66bb24b0fc8806
asror2727 <asrorake001@gmail.com>
2026-07-21 13:03:14 +0500

    package.js

---
+++ package.js
@@ -0,0 +1,15 @@
+{
+  "name": "mafia-bot",
+  "version": "1.0.0",
+  "description": "Telegram Mafia guruh o'yini boti",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "dotenv": "^16.4.5",
+    "express": "^4.19.2",
+    "mongoose": "^8.4.0",
+    "telegraf": "^4.16.3"
+  }
+}
