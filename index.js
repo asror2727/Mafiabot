ce15b035deb45f30cb6c2b4c13d44fa95c0dfa0b
asror2727 <asrorake001@gmail.com>
2026-07-24 21:24:47 +0500

    Refactor index.js to simplify server setup
    Removed dotenv configuration and MongoDB connection. Updated response message and simplified server start log.

--- index.js
+++ index.js
@@ -1,28 +1,13 @@
-require("dotenv").config();
-
 const express = require("express");
-const connectDB = require("./config/db");
 
 const app = express();
 
 app.use(express.json());
 
-// MongoDB ulash
-connectDB();
-
 app.get("/", (req, res) => {
-
-  res.json({
-    ok: true,
-    message: "Mafia Engine Online 🚀"
-  });
-
+  res.send("✅ Mafia Server Online");
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-
-  console.log("Server Started : " + PORT);
-
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server Started");
 });
